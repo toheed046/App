f1c55343bb6df6dfa6cd303240cc320b0049e5c5
SANDEEP RAWAT <SANDEEP RAWAT@SANDEEP>
2018-11-30 15:09:04 +0530

    Test.java file is updated

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -2,4 +2,14 @@ package com.app;
 
 public class Test {
 
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
